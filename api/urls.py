845ede6989834e67f93f8eb2957d8ab5b98361c4
Anthony <anthonyd-m@live.com>
2019-10-24 15:18:54 +0200

    added models and serializers for testing

--- api/urls.py
+++ api/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api_app import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', urls),
 ]
